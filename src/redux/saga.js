d8c0087634808b42e2872aaf9c3b790ef234b671
aradhanasinghmcscet <aradhanasinghmcscet@gmail.com>
2023-01-12 08:44:38 +0530

    node-modules and build deleted

---
+++ src/redux/saga.js
@@ -0,0 +1,8 @@
+// import { all, fork } from "redux-saga/effects";
+// import todo from "../task/saga";
+
+// function* rootSaga() {
+//   yield all([todo].map(fork));
+// }
+
+// export default rootSaga;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import {createStore} from 'redux';
+// import {createStore} from 'redux';
+import { configureStore } from "@reduxjs/toolkit";
+// import createSagaMiddleware from "redux-saga";
 import { rootReducers } from "./rootReducer";
 
-export const store = createStore(
-    rootReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+export const store = configureStore({
+    reducer: rootReducers,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     // can configure redux devtool
-)
+});
